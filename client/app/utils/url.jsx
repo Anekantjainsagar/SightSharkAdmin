a6ed79cebc71b1d4c74aa8c3bb2d641152ea6c3c
AshisMaity <maity.ashis651@gmail.com>
2024-11-02 13:12:18 +0530

    add backend url

--- client/app/utils/url.jsx
+++ client/app/utils/url.jsx
@@ -1,8 +1,3 @@
 
 
-// console.log("process.env.NEXT_PUBLIC_BACKEND_URI", process.env.NEXT_PUBLIC_BACKEND_URI);
-
-
-
-export const BACKEND_URI = process.env.NEXT_PUBLIC_BACKEND_URI;
-  // "https://admin-portal-backend-118842008879.asia-south1.run.app";
+export const BACKEND_URI = "https://admin-portal-backend-118842008879.asia-south1.run.app";
