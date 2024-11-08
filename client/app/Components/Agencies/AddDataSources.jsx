96a9fd7fefac279410cfccf9e51687d84be9aeec
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-11-08 20:42:37 +0530

    Border added in data sources

--- client/app/Components/Agencies/AddDataSources.jsx
+++ client/app/Components/Agencies/AddDataSources.jsx
@@ -142,7 +142,7 @@ const AddDataSouces = ({ showSubscribe, setShowSubscribe, original_data }) => {
                                 e?.name?.slice(1)?.replaceAll("_", " ")}
                             </p>
                           </div>
-                          <div className="inline-flex items-start mr-1">
+                          <div className="inline-flex items-start mr-1 w-3/12 justify-end border">
                             <label className="relative flex items-center cursor-pointer">
                               <input
                                 type="checkbox"
