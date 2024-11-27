dafc766841a1e72f65f5f5f7047dcf81b38aa23a
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-11-27 15:55:49 +0530

    Data sources page change

--- client/app/data-sources/page.jsx
+++ client/app/data-sources/page.jsx
@@ -5,6 +5,7 @@ import Navbar from "@/app/Components/Utils/Navbar";
 import Context from "../Context/Context";
 import Image from "next/image";
 import { IoReload } from "react-icons/io5";
+import { TfiReload } from "react-icons/tfi";
 
 function formatName(input) {
   return input
@@ -26,79 +27,63 @@ const DataSources = () => {
         <div className="absolute backdrop-blur-3xl top-0 left-0 w-full h-full px-5 overflow-y-auto">
           <Navbar />
           <div className="text-white w-full rounded-lg py-2 px-6 min-[1600px]:py-6">
-            <h3 className="text-xl min-[1600px]:text-2xl font-semibold">
-              Agencies Data Sources{" "}
-              <span className="text-lg min-[1600px]:text-xl text-white/80">
-                ({agencies?.total_count})
-              </span>
-            </h3>
-            <div className="mt-5 h-[78vh] rounded-2xl">
-              <div className="h-[90%] overflow-y-auto small-scroller">
-                {agencies?.data?.map((e, i) => {
-                  return (
-                    <div
-                      key={i}
-                      className="border px-6 py-5 mb-10 rounded-2xl border-gray-200/5"
-                    >
-                      <div className="flex items-center justify-between">
-                        <h5 className="text-xl">{e?.agency_name}</h5>
-                        <button className="bg-newBlue text-white rounded-md px-4 py-1">
-                          Refresh Data Sources
-                        </button>
-                      </div>
-                      <div className="grid grid-cols-7 gap-8 mt-5">
-                        {datasources?.map((e, i) => {
-                          return (
-                            <div key={i}>
-                              <div className="video-box-shadow py-10 rounded-2xl hover:rounded-xl hover:scale-105 transition-all cursor-pointer flex flex-col text-white justify-center items-center lg:px-0 px-1 h-fit">
-                                <Image
-                                  src={e?.img_link}
-                                  alt={e?.img_link?.src}
-                                  width={1000}
-                                  height={1000}
-                                  className="aspect-squre object-contain w-2/12"
-                                />{" "}
-                                <p className="text-base min-[1600px]:text-lg cursor-pointer mt-2">
-                                  {formatName(e?.name)}
-                                </p>
-                              </div>
-                              <div className="mt-4 flex items-start justify-between px-2">
-                                <p className="text-xs min-[1600px]:text-sm cursor-pointer">
-                                  Last Refresh Date & Time
-                                  <br />
-                                  {new Date(Date.now()).toString().slice(4, 21)}
-                                </p>
-                                <IoReload className="text-lg cursor-pointer" />
-                              </div>
-                            </div>
-                          );
-                        })}
-                      </div>
+            <div className="flex items-center justify-between">
+              <h3 className="text-xl min-[1600px]:text-2xl font-semibold">
+                Agencies Data Sources{" "}
+                <span className="text-lg min-[1600px]:text-xl text-white/80">
+                  ({agencies?.total_count})
+                </span>
+              </h3>
+              <div></div>
+            </div>
+            <div className="overflow-y-auto mt-5 h-[78vh] rounded-2xl small-scroller">
+              {agencies?.data?.map((e, i) => {
+                return (
+                  <div
+                    key={i}
+                    className="border px-6 py-5 mb-10 rounded-2xl border-gray-200/5"
+                  >
+                    <div className="flex items-center justify-between">
+                      <h5 className="text-2xl">{e?.agency_name}</h5>
+                      <button className="bg-newBlue text-white flex items-center gap-x-2 rounded-lg px-6 py-3">
+                        <TfiReload />
+                        Refresh All
+                      </button>
                     </div>
-                  );
-                })}{" "}
-              </div>
-              <div className="h-[10%] gap-x-4  px-6 flex items-center justify-center bg-[#030021]/40 rounded-2xl">
-                {[...Array(agencies?.total_pages).keys()]
-                  .map((i) => i + 1)
-                  ?.map((e, i) => {
-                    return (
-                      <div
-                        className={`w-[30px] cursor-pointer min-[1600px]:w-[40px] h-[30px] text-sm min-[1600px]:text-base min-[1600px]:h-[40px] rounded-lg flex items-center justify-center ${
-                          agencies?.current_page == e
-                            ? "bg-newBlue"
-                            : "text-[#85888E]"
-                        }`}
-                        key={i}
-                        onClick={() => {
-                          getAgencies(e);
-                        }}
-                      >
-                        {e}
-                      </div>
-                    );
-                  })}
-              </div>
+                    <div className="grid grid-cols-7 gap-8 mt-5">
+                      {datasources?.map((e, i) => {
+                        return (
+                          <div
+                            key={i}
+                            className="border border-gray-400/20 rounded-2xl p-2"
+                          >
+                            <div className="py-10 border border-gray-400/20 rounded-2xl hover:rounded-xl hover:scale-105 transition-all cursor-pointer flex flex-col text-white justify-center items-center lg:px-0 px-1 h-fit">
+                              <Image
+                                src={e?.img_link}
+                                alt={e?.img_link?.src}
+                                width={1000}
+                                height={1000}
+                                className="aspect-squre object-contain w-2/12"
+                              />{" "}
+                              <p className="text-base min-[1600px]:text-lg cursor-pointer mt-2">
+                                {formatName(e?.name)}
+                              </p>
+                            </div>
+                            <div className="mt-2 flex items-end justify-between px-2">
+                              <p className="text-[10px] min-[1600px]:text-xs cursor-pointer">
+                                Last Refresh Time
+                                <br />
+                                {new Date(Date.now()).toString().slice(4, 21)}
+                              </p>
+                              <IoReload className="text-lg cursor-pointer" />
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                );
+              })}{" "}
             </div>
           </div>
         </div>
