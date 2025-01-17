cb964ae5bd564cb9a240e06e7a96bb87dd7b4cb5
Anekantjainsagar <anekantjainsagar@gmail.com>
2025-01-17 13:26:14 +0530

    Updatd

--- client/app/Components/Agencies/AddMainTemplates.jsx
+++ client/app/Components/Agencies/AddMainTemplates.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import Modal from "react-modal";
 import { AiOutlineClose } from "react-icons/ai";
 import toast from "react-hot-toast";
@@ -8,6 +8,7 @@ import { BACKEND_URI } from "@/app/utils/url";
 import { getCookie } from "cookies-next";
 import Context from "@/app/Context/Context";
 import axios from "axios";
+import Image from "next/image";
 
 const customStyles = {
   overlay: { zIndex: 50 },
@@ -24,15 +25,62 @@ const customStyles = {
 };
 
 const AddMainTemplates = ({ showSubscribe, setShowSubscribe }) => {
+  const fileInputRef = useRef(null);
+  const [file, setFile] = useState();
   const { getAllTemplates } = useContext(Context);
+  const [isDragging, setIsDragging] = useState(false);
   const [data, setData] = useState({
     template_name: "",
     template_link: "",
+    template_image: "",
   });
-
   function closeModal() {
     setShowSubscribe(false);
   }
+
+  const handleFileChangeProfile = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setFile(URL.createObjectURL(file));
+      setData({ ...data, template_image: file }); // Update `data` state with the selected file
+    } else {
+      console.log("No file selected");
+    }
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDragging(false);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setIsDragging(false);
+    const droppedFile = e.dataTransfer.files[0];
+    if (droppedFile) {
+      handleFileChangeProfile({ target: { files: [droppedFile] } });
+    }
+  };
+
+  const handlePaste = (e) => {
+    const clipboardItem = e.clipboardData.items[0];
+    if (clipboardItem && clipboardItem.type.startsWith("image")) {
+      const pastedFile = clipboardItem.getAsFile();
+      handleFileChangeProfile({ target: { files: [pastedFile] } });
+    }
+  };
+
+  const handleClearProfile = () => {
+    setFile(null);
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+      setData({ ...data, template_image: "" });
+    }
+  };
 
   return (
     <div className="z-50">
@@ -52,6 +100,69 @@ const AddMainTemplates = ({ showSubscribe, setShowSubscribe }) => {
             <h1 className="text-3xl font-medium text-center mb-4">
               Add New Template
             </h1>
+            <div className="flex items-center justify-center">
+              <div
+                className={`relative flex w-full flex-col items-center justify-center ${
+                  isDragging
+                    ? "border-2 border-dashed border-blue-500 bg-black/30 cursor-pointer"
+                    : ""
+                }`}
+                onDragOver={handleDragOver}
+                onDragLeave={handleDragLeave}
+                onDrop={handleDrop}
+                onPaste={handlePaste}
+              >
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  style={{ display: "none" }}
+                  onChange={handleFileChangeProfile}
+                />
+                <div className="relative">
+                  <div
+                    onClick={() => {
+                      if (file) {
+                        handleClearProfile();
+                      } else {
+                        fileInputRef.current.click();
+                      }
+                    }}
+                    title={
+                      !file ? "Upload Template Image" : "Remove Template Image"
+                    }
+                    className="absolute bg-newBlue flex items-center justify-center text-2xl px-2 top-6 aspect-square cursor-pointer right-6 rounded-full"
+                  >
+                    {file ? (
+                      <AiOutlineClose className="text-base md:text-lg" />
+                    ) : (
+                      "+"
+                    )}
+                  </div>
+                  <Image
+                    src={file ? file : "/Agency/temp_logo.png"}
+                    alt="Agency Img"
+                    width={1000}
+                    height={1000}
+                    className="w-[6vw] min-[1600px]:w-[15vw] aspect-square object-cover rounded-full border border-gray-100/10"
+                    title={
+                      !file ? "Upload Template Image" : "Remove Template Image"
+                    }
+                  />
+                </div>
+                {
+                  <p className="text-center mt-3 text-gray-300">
+                    {data?.template_image?.name || (
+                      <span className="opacity-0">hell</span>
+                    )}
+                  </p>
+                }
+                {isDragging && (
+                  <p className="absolute text-blue-500 mt-3">
+                    Drop file to upload
+                  </p>
+                )}
+              </div>
+            </div>
             <div className="flex flex-col w-full">
               <label
                 htmlFor="name"
@@ -143,38 +254,146 @@ export const UpdateMainTemplates = ({
   setShowSubscribe,
   original_data,
 }) => {
+  const fileInputRef = useRef(null);
+  const [file, setFile] = useState();
   const { getAllTemplates } = useContext(Context);
+  const [isDragging, setIsDragging] = useState(false);
   const [data, setData] = useState({
     template_name: "",
     template_link: "",
+    template_image: "",
   });
-
-  function closeModal() {
-    setShowSubscribe(false);
-  }
 
   useEffect(() => {
     setData({ ...original_data });
+    setFile(original_data?.template_image);
   }, [original_data]);
+
+  const handleFileChangeProfile = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setFile(URL.createObjectURL(file));
+      setData({ ...data, template_image: file }); // Update `data` state with the selected file
+    } else {
+      console.log("No file selected");
+    }
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDragging(false);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setIsDragging(false);
+    const droppedFile = e.dataTransfer.files[0];
+    if (droppedFile) {
+      handleFileChangeProfile({ target: { files: [droppedFile] } });
+    }
+  };
+
+  const handlePaste = (e) => {
+    const clipboardItem = e.clipboardData.items[0];
+    if (clipboardItem && clipboardItem.type.startsWith("image")) {
+      const pastedFile = clipboardItem.getAsFile();
+      handleFileChangeProfile({ target: { files: [pastedFile] } });
+    }
+  };
+
+  const handleClearProfile = () => {
+    setFile(null);
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+      setData({ ...data, template_image: "" });
+    }
+  };
 
   return (
     <div className="z-50">
       <Modal
         isOpen={showSubscribe}
-        onRequestCl2ose={closeModal}
+        onRequestClose={() => setShowSubscribe(false)}
         style={customStyles}
         contentLabel="Example Modal"
       >
         <div className="relative rounded-lg bg-main pt-10 text-white">
           <AiOutlineClose
             size={40}
-            onClick={closeModal}
+            onClick={() => setShowSubscribe(false)}
             className="absolute top-2 right-2 px-2 cursor-pointer"
           />
           <div className="px-[4vw] w-[30vw] gap-y-5 grid">
             <h1 className="text-3xl font-medium text-center mb-4">
               Update Template
-            </h1>
+            </h1>{" "}
+            <div className="flex items-center justify-center">
+              <div
+                className={`relative flex w-full flex-col items-center justify-center ${
+                  isDragging
+                    ? "border-2 border-dashed border-blue-500 bg-black/30 cursor-pointer"
+                    : ""
+                }`}
+                onDragOver={handleDragOver}
+                onDragLeave={handleDragLeave}
+                onDrop={handleDrop}
+                onPaste={handlePaste}
+              >
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  style={{ display: "none" }}
+                  onChange={handleFileChangeProfile}
+                />
+                <div className="relative">
+                  <div
+                    onClick={() => {
+                      if (file) {
+                        handleClearProfile();
+                      } else {
+                        fileInputRef.current.click();
+                      }
+                    }}
+                    title={
+                      !file ? "Upload Template Image" : "Remove Template Image"
+                    }
+                    className="absolute bg-newBlue flex items-center justify-center text-2xl px-2 top-6 aspect-square cursor-pointer right-6 rounded-full"
+                  >
+                    {file ? (
+                      <AiOutlineClose className="text-base md:text-lg" />
+                    ) : (
+                      "+"
+                    )}
+                  </div>
+                  <Image
+                    src={file ? file : "/Agency/temp_logo.png"}
+                    alt="Agency Img"
+                    width={1000}
+                    height={1000}
+                    className="w-[6vw] min-[1600px]:w-[15vw] aspect-square object-cover rounded-full border border-gray-100/10"
+                    title={
+                      !file ? "Upload Template Image" : "Remove Template Image"
+                    }
+                  />
+                </div>
+                {
+                  <p className="text-center mt-3 text-gray-300">
+                    {data?.template_image?.name || (
+                      <span className="opacity-0">hell</span>
+                    )}
+                  </p>
+                }
+                {isDragging && (
+                  <p className="absolute text-blue-500 mt-3">
+                    Drop file to upload
+                  </p>
+                )}
+              </div>
+            </div>
             <div className="flex flex-col w-full">
               <label
                 htmlFor="name"
