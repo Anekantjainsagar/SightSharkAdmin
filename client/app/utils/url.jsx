9e0a48302fd85c4dfead11aec38d2a431900f374
Sha <admin.sightshark.com>
2025-01-27 09:11:23 +0000

    updated url and nexjs version

--- client/app/utils/url.jsx
+++ client/app/utils/url.jsx
@@ -1,5 +1,4 @@
-// export const BACKEND_URI = "https://sightshark-backend.onrender.com";
 
 
 export const BACKEND_URI = process.env.NEXT_PUBLIC_BACKEND_URI;
-// export const BACKEND_URI = "https://admin-portal-backend-118842008879.asia-south1.run.app";
+
